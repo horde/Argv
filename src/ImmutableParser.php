74a5618cc4d6cd32a0e6a879bb71ef5d8acf2b52
Ralf Lang <ralf.lang@ralf-lang.de>
2026-03-04 12:11:48 +0100

    docs: mark under-development stub classes as internal
    Add @internal and @deprecated warnings to stub classes that are
    under development and not ready for production use:
    - ArgvParser interface
    - ImmutableParser
    - ParserArgument
    - ParserBuilder
    - ParserOption
    - ParserResult
    
    These stubs will be evolved during Modern API implementation.

--- src/ImmutableParser.php
+++ src/ImmutableParser.php
@@ -4,6 +4,15 @@ namespace Horde\Argv;
 /**
  * An immutable parser can only configured once through its constructor
  * and provides no interface for later modification.
+ *
+ * @category Horde
+ * @package  Argv
+ * @author   Ralf Lang <ralf.lang@ralf-lang.de>
+ * @license  http://www.horde.org/licenses/bsd BSD
+ *
+ * @internal
+ * @deprecated This class is under development and not ready for production use.
+ *             The API is incomplete and may change without notice. Do not use in production code.
  */
 class ImmutableParser implements ArgvParser
 {
